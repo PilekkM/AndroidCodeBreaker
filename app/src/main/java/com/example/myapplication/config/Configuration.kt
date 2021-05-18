fe3bc7e89bac83463d618f644d877fe441f62f8d
Piotr <pilek97@gmail.com>
2021-05-18 20:17:02 +0200

    Corrected typo in log directory name

--- app/src/main/java/com/example/myapplication/config/Configuration.kt
+++ app/src/main/java/com/example/myapplication/config/Configuration.kt
@@ -5,6 +5,6 @@ class Configuration {
         const val PASSWORD_TO_CHECK = "12345"
         const val SENSOR_ACCURACY_MILLIS = 1000L
         const val GRAVITY_SENSOR_LOG_FILENAME = "gravity_sensor_log"
-        const val LOG_DIR_NAME = "codebraker"
+        const val LOG_DIR_NAME = "codebreaker"
     }
 }
