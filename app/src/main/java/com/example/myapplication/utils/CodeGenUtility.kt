9e393dfd79ad869be32fb3c93c5a68a41cb9c20e
Piotr <pilek97@gmail.com>
2021-05-29 08:48:20 +0200

    Added comments with review in CodeGenUtility

--- app/src/main/java/com/example/myapplication/utils/CodeGenUtility.kt
+++ app/src/main/java/com/example/myapplication/utils/CodeGenUtility.kt
@@ -4,10 +4,13 @@ class CodeGenUtility {
     companion object{
         var CURRENT_PASS = "1234"
         fun genNewPassword(): CharSequence {
-//            val pass: String = ""
+//            val pass: String = "" //you don't have to write that this is String, Kotlin will figure this out. 'var pass = ""' should work fine.
 //            for (i in 1..4)
-//                val pass = pass + (0..9).random()
+//                val pass = pass + (0..9).random() //it is clearer to use "pass += value" instead of "pass = pass + value"
             // TODO: Why the above does not work?!
+            // val is used for variables which won't be reassigned. If you want to assign new value to variable after initializing it you should
+            // use 'var' - above code should work if you switch val for var in line 7 and won't use var or val in line 9(you don't have and shouldn't
+            // do this because you want to assign value to already existing variable).
             val a = (0..9).random()
             val b = (0..9).random()
             val c = (0..9).random()
